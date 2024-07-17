81360fada4215ce8e494e36a06b04d4a394eece9
A-peaches <geunmi0521@gmail.com>
2024-07-17 11:44:07 +0900

    background color

--- Vue/mile/src/App.vue
+++ Vue/mile/src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <Header v-if="!$route.meta.hideHeader" /> <!--특정 라우터에서 Header를 숨기는 메타 필드 추가-->
-  <router-view />
+  <div id="app">
+    <main v-if="!$route.meta.hideHeader">
+      <Header v-if="!$route.meta.hideHeader" />
+      <router-view />
+    </main>
+    <router-view v-else /> <!-- hideHeader가 true일 때는 main과 Header를 숨기고 router-view만 렌더링 -->
+  </div>
 </template>
-
 <script>
 import { mapActions } from 'vuex';
 import Header from './views/test/HeaderView.vue';
--- Vue/mile/src/views/user/main/UserMainView.vue
+++ Vue/mile/src/views/user/main/UserMainView.vue
@@ -1,16 +1,36 @@
 <template>
-  <h1>여기는 유저 메인 페이지 입니다.</h1>
-  <h3>유저 프로필</h3>
-  <h3>나의 마일리지 버튼</h3>
-  <h3>오늘의 추천</h3>
-  <h3>출석체크</h3>
-  <h3>즐겨찾기 마일리지</h3>
-  <h3>별별 마일리지</h3>
+  <div class="flex" style="margin-left: 10%; margin-right: 10%;">
+    <div class="cards" style="width: 24% ;height : 400px">
+      <img
+        src="@\assets\test.png"
+        class="profile-large my-3"
+        alt="Profile Picture"
+      />
+      <h2 class="lg KB_S5 my-3">{{loginInfo.user_name}}</h2>
+      <p class="md" style="margin-bottom: 0px">{{loginInfo.level_no}} {{loginInfo.position_no}} | {{loginInfo.job_no}}직무</p>
+      <p class="md" style="margin-bottom: 10px">{{ loginInfo.dp_no }}</p>
+      <button class="btn-yellow KB_C2">나의 마일리지</button>
+    </div>
+    <favorite  style="width: 70%; margin-left:3%"/>
+  </div>
 </template>
-
+asw nns dx
 <script>
+import favorite from '@/components/main/FavoriteCom.vue'
+import { mapGetters } from 'vuex';
+
 export default {
-  name: 'UserMainView',
-  components: {},
+  name: "UserMainView",
+  components: { favorite },
+  methods :{
+  
+  },
+  computed :{
+    ...mapGetters('login', ['getLoginInfo']),
+    
+    loginInfo() {
+      return this.getLoginInfo;
+    }
+  }
 };
 </script>
