4c9b7d720bc4bee1182574f117f20290027741cf
A-peaches <geunmi0521@gmail.com>
2024-07-17 12:42:57 +0900

    add button

--- SpringBoot/src/main/java/com/kbstar/mileEasy/mapper/UserDao.java
+++ SpringBoot/src/main/java/com/kbstar/mileEasy/mapper/UserDao.java
@@ -11,8 +11,4 @@ public interface UserDao {
     User selectUserById(String user_no);
     ArrayList<User> selectAllUser();
     User checkedUser(String user_no, String user_pw);
-
-    Optional<User> findByEmployeeNumberAndEmail(@Param("employeeNumber") String employeeNumber, @Param("email") String email);
-
-    void updatePassword(@Param("user_no") String user_no, @Param("password") String password);
 }
--- Vue/mile/src/components/main/FavoriteCom.vue
+++ Vue/mile/src/components/main/FavoriteCom.vue
@@ -2,12 +2,20 @@
     <div class="cards" style="background-color: #f9f9f9; height: 400px">
         <p class="text-left lg2 KB_C2">즐겨찾기 마일리지</p>
         <div class="flex">
-            <div class="cards favorite-card"><i class="bi bi-plus-lg" style="color:black"></i></div>
-            <div class="cards favorite-card">뀨</div>
+            <div class="cards favorite-card">
+                <img src="@/assets/img/add.png" class="addImg">
+            </div>
+            <div class="cards favorite-card">
+                <img src="@/assets/img/add.png" class="addImg">
+            </div>
         </div>
         <div class="flex">
-            <div class="cards favorite-card">뀨</div>
-            <div class="cards favorite-card">뀨</div>
+            <div class="cards favorite-card">
+                <img src="@/assets/img/add.png" class="addImg">
+            </div>
+            <div class="cards favorite-card">
+                <img src="@/assets/img/add.png" class="addImg">
+            </div>
         </div>
      </div>   
 </template>
@@ -15,7 +23,13 @@
 <style>
 .favorite-card {
     width: 100%;
+    display: flex;
     height : 150px;
+    justify-content: center; /* 가로 중앙 정렬 */
+    align-items: center; /* 세로 중앙 정렬 */
 }
 
+.addImg {
+    width:18%;
+}
 </style>
