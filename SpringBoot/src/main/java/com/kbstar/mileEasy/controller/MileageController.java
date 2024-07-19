b009426b51ffce50ba27d19a134340627ee1c282
A-peaches <geunmi0521@gmail.com>
2024-07-19 15:22:21 +0900

    추천멘트최종

--- SpringBoot/src/main/java/com/kbstar/mileEasy/controller/MileageController.java
+++ SpringBoot/src/main/java/com/kbstar/mileEasy/controller/MileageController.java
@@ -1,6 +1,5 @@
 package com.kbstar.mileEasy.controller;
 
-
 import com.kbstar.mileEasy.dto.*;
 import com.kbstar.mileEasy.service.mileage.info.HitMileService;
 import com.kbstar.mileEasy.service.mileage.info.MileHistoryService;
@@ -66,13 +65,12 @@ public class MileageController {
         hitMileService.hitMile(mile_no);
     }
 
-
     //마일리지 추천멘트!
     @GetMapping("getRecommand/{user_no}")
-    public List<MileRecommand> getRecommand(@PathVariable String user_no) {
-        List<MileRecommand> recommandsList = mileService.getRecommand(user_no);
-        
-    }
+    public MileRecommand getRecommand(@PathVariable String user_no) {
+        MileRecommand recommands = mileService.getRecommand(user_no);
 
+        return recommands;
+    }
 
 }
