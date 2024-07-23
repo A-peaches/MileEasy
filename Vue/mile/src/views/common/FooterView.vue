35949ea672f0e63a2b420da8af3a5701d3a795af
A-peaches <geunmi0521@gmail.com>
2024-07-23 16:52:31 +0900

    푸터 임시추가

---
+++ Vue/mile/src/views/common/FooterView.vue
@@ -0,0 +1,25 @@
+<template>
+<!-- Footer -->
+<footer style=" padding: 20px; text-align: center; height : 250px;
+background-color : #fbfbfb">
+    <hr>
+    <div style=" margin-top: 50px; margin-bottom: 10px;">
+      <img src="footer-logo.png" alt="MileEasy Logo" style="height: 40px;">
+    </div>
+    <div style="margin-bottom: 10px;">
+      <a href="/about" style="margin-right: 20px;">About Us</a>
+      <a href="/contact" style="margin-right: 20px;">Contact</a>
+      <a href="/privacy" style="margin-right: 20px;">Privacy Policy</a>
+      <a href="/terms" style="margin-right: 20px;">Terms of Service</a>
+    </div>
+    <div>
+      &copy; 2024 MileEasy. All rights reserved.
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+    name: "FooterView",
+
+}
+</script>
--- Vue/mile/src/views/user/main/UserMainView.vue
+++ Vue/mile/src/views/user/main/UserMainView.vue
@@ -1,53 +1,34 @@
 <template>
-  <div class="flex" style="margin-left: 10%; margin-right: 10%">
-    <div class="cards" style="width: 24%; height: 430px">
-      <img
-        src="@/assets/img/test.png"
-        class="profile-large my-3"
-        alt="Profile Picture"
-      />
-      <h2 class="lg KB_S5 my-3">{{ loginInfo ? loginInfo.user_name : '' }}</h2>
-      <p class="md" style="margin-bottom: 0px">
-        {{
-          loginInfo
-            ? `${loginInfo.level_no} ${loginInfo.position_no} | ${loginInfo.job_no} 직무`
-            : ''
-        }}
-      </p>
-      <p class="md mb-2" style="margin-bottom: 0px">
-        {{ loginInfo ? `${loginInfo.dp_no}` : '' }}
-      </p>
-      <button class="btn-yellow KB_C2 my-3">나의 마일리지</button>
-    </div>
-    <favorite style="width: 70%; margin-left: 3%" />
-  </div>
-
-  <!-- 추천 멘트 및 마일리지 차트 -->
-  <div
-    class="flex-container"
-    style="
-      margin-left: 10%;
-      margin-right: 10%;
-      display: flex;
-      align-items: flex-start;
-    "
-  >
-    <!-- 오늘의 추천 및 출석체크 -->
-    <div
-      class="left-column"
-      style="
-        width: 24%;
-        display: flex;
-        flex-direction: column;
-        margin-right: 2%;
-      "
-    >
+  <div class="flex" style="margin-left: 10%; margin-right: 10%  " >
+    <!-- 나의 마일리지, 추천 멘트, 출석 체크 묶음 -->
+    <div class="left-container" style="width: 24%; margin-right: 1%">
+      <div class="cards" style="height: 430px">
+        <img
+          src="@/assets/img/test.png"
+          class="profile-large my-3"
+          alt="Profile Picture"
+        />
+        <h2 class="lg KB_S5 my-3">{{ loginInfo ? loginInfo.user_name : '' }}</h2>
+        <p class="md" style="margin-bottom: 0px">
+          {{
+            loginInfo
+              ? `${loginInfo.level_no} ${loginInfo.position_no} | ${loginInfo.job_no} 직무`
+              : ''
+          }}
+        </p>
+        <p class="md mb-2" style="margin-bottom: 0px">
+          {{ loginInfo ? `${loginInfo.dp_no}` : '' }}
+        </p>
+        <button class="btn-yellow KB_C2 my-3">나의 마일리지</button>
+      </div>
       <recommand class="my-5" style="height: 210px" v-if="loginInfo" />
       <attendance style="height: 395px" v-if="loginInfo" />
     </div>
-    <!-- 뀨! -->
-    <div class="right-column" style="flex: 1; margin-left: 3%; height: 430px">
-      <milageCharts class="my-5" style="height: 100%" v-if="loginInfo" />
+
+    <!-- 즐겨찾기, 마일리지 차트 묶음 -->
+    <div class="right-container" style="width: 70%; margin-left: 1%; position: sticky; top: 20px;  height: 100vh;">
+      <favorite style="width: 100%; margin-bottom: 49px;" />
+      <milageCharts style="width: 100%; height: 430px" v-if="loginInfo" />
     </div>
   </div>
 </template>
