4382bc0ab57586dddba022fbacb37067f1e4c9dc
A-peaches <geunmi0521@gmail.com>
2024-08-02 18:11:09 +0900

    main 끝

---
+++ Vue/mile/src/components/m-tip/BestM-TipCom.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="cards" style="height: 400px; margin-top:20px;">
+        뀨뀨
+     </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BestM-TipCom',
+    data() {
+
+  },
+  methods :{
+ 
+  }
+  };
+  </script>
+  
+  <style scoped>
+
+
+  </style>
---
+++ Vue/mile/src/components/m-tip/GuideCom.vue
@@ -0,0 +1,45 @@
+<template>
+    <div key="profile" class="cards" style="height: 105%; padding-left:30px;
+    margin-top:20px;">
+      이곳은 바로 M-Tip 가이드를 불러오세요!!<br>
+      이곳은 바로 M-Tip 가이드를 불러오세요!!<br>
+      이곳은 바로 M-Tip 가이드를 불러오세요!!<br>
+      이곳은 바로 M-Tip 가이드를 불러오세요!!<br>
+      이곳은 바로 M-Tip 가이드를 불러오세요!!<br>
+    </div>
+  
+  
+  </template>
+  
+  <script>
+  import { mapGetters } from "vuex";
+  
+  export default {
+    name: "GuideCom",
+    data() {
+     
+    },
+    computed: {
+      ...mapGetters("login", ["getLoginInfo"]),
+      loginInfo() {
+        const info = this.getLoginInfo;
+        return info
+          ? {
+              ...info,
+              position_no: info.position_no || "",
+              level_no: info.level_no || "",
+              job_no: info.job_no || "",
+              dp_no: info.dp_no || "",
+            }
+          : null;
+      },
+    },
+    methods: {
+
+    },
+  };
+  </script>
+  
+  <style scoped>
+
+  </style>
