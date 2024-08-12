d45faf20aa45c82ca66d58d9dae41ab01095bde3
A-peaches <geunmi0521@gmail.com>
2024-08-12 09:35:44 +0900

    merging

--- SpringBoot/src/main/java/com/kbstar/mileEasy/controller/CustomBotController.java
+++ SpringBoot/src/main/java/com/kbstar/mileEasy/controller/CustomBotController.java
@@ -38,4 +38,13 @@ public class CustomBotController {
         return chatGPTResponse.getChoices().get(0).getMessage().getContent();
     }
 
+    @PostMapping("/chatMMS")
+    public String PostchatMMS(@RequestParam(name = "prompt") String prompt){
+        ChatGPTRequest request = new ChatGPTRequest(model, prompt);
+        ChatGPTResponse chatGPTResponse =  template.postForObject(apiURL, request, ChatGPTResponse.class);
+
+        assert chatGPTResponse != null;
+        return chatGPTResponse.getChoices().get(0).getMessage().getContent();
+    }
+
 }
